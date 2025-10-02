1ea8926263f667ccec5f9f194455ec20fb62deb8
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2025-10-02 15:41:57 +0200

    update: added TestDTO

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,9 +1,16 @@
+using WebApplication2;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet(
     "/", 
     () => Results.Ok("Hello, World!")
+);
+
+app.MapPost(
+    "/",
+    (TestDTO dto) => Results.Ok($"Hello, {dto.Name}, you are {dto.Age} years old.")
 );
 
 app.Run();
---
+++ WebApplication2/TestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2
+{
+    public class TestDTO
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
